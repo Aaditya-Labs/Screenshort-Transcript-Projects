0a0aded5d34bbfd6da8d4e1f9ddfd6393c26daba
Aditya Kumar Singh <139832003+Aaditya-Labs@users.noreply.github.com>
2025-10-22 20:13:38 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
+import threading
+from gui import run_gui
+from api import app
+import os
+
+if __name__ == "__main__":
+    os.makedirs("screenshots", exist_ok=True)
+    
+    # Run both GUI and API concurrently
+    threading.Thread(target=lambda: app.run(debug=False, port=5000)).start()
+    run_gui()
 
